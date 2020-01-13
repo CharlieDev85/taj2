20be1236c13def1ff73ca93f68bcb576227337f5
charlie <charliemarroq@gmail.com>
2020-01-12 23:55:47 -0600

    trying jpatest to make circleci works

--- src/test/java/com/taj/demo/DemotajApplicationTests.java
+++ src/test/java/com/taj/demo/DemotajApplicationTests.java
@@ -1,11 +1,14 @@
 package com.taj.demo;
 
 //Testing springboot from course
+
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 
 @SpringBootTest
+@DataJpaTest
 public class DemotajApplicationTests {
     @Test
     public void contextLodas(){
